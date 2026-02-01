2536ae6c5d37bb14a6e30ac4745701939b3fd471
alpha3002025 <alpha300uk@gmail.com>
2026-02-01 15:17:53 +0900

    feat: backend, init, provider

---
+++ terraform/init/init.tf
@@ -0,0 +1,21 @@
+resource "aws_s3_bucket" "tfstate" {
+  bucket = "overtake-eks-apnortheast2-tfstate" ## (1)
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.tfstate.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name         = "overtake-eks-terraform-lock" ## (2)
+  hash_key     = "LockID"
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
