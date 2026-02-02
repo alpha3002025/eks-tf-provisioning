dee5de1ce6fdedf93415b0274a6ce68bd5e0c769
alpha3002025 <alpha300uk@gmail.com>
2026-02-02 15:14:28 +0900

    feat: vpc setup

---
+++ terraform/vpc/eksd_apnortheast2/route_table_route.tf
@@ -0,0 +1,11 @@
+resource "aws_route" "public_internet_gateway" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.default.id
+}
+
+resource "aws_route" "private_nat" {
+  count                  = length(var.availability_zones)
+  route_table_id         = aws_route_table.private[count.index].id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat[count.index].id
