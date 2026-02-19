6855821050ba8d6c34a2a12c96829350e3ea25ea
alpha3002025 <alpha300uk@gmail.com>
2026-02-19 10:39:34 +0900

    feat: new-karpenter

--- terraform/eks/eksd_apnortheast2/eksdapne2-eusc/provider.tf
+++ terraform/eks/eksd_apnortheast2/eksdapne2-eusc/provider.tf
@@ -29,24 +29,20 @@ provider "aws" {
   }
 }
 
+data "aws_eks_cluster_auth" "cluster" {
+  name = local.cluster_name
+}
+
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = concat(["eks", "get-token", "--cluster-name", local.cluster_name], local.role_args)
-    command     = "aws"
-  }
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = concat(["eks", "get-token", "--cluster-name", local.cluster_name], local.role_args)
-      command     = "aws"
-    }
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
