d0eec618729ca1f5cbe4943915f7bc9269eea911
alpha3002025 <alpha300uk@gmail.com>
2026-02-19 10:49:53 +0900

    fix: karpenter 관련 수정내용

--- terraform/eks/eksd_apnortheast2/eksdapne2-wkqo/provider.tf
+++ terraform/eks/eksd_apnortheast2/eksdapne2-wkqo/provider.tf
@@ -20,3 +20,21 @@ provider "kubernetes" {
     command     = "aws"
   }
 }
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = local.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
